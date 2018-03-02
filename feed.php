58dcb84042bd72399d7ab60e0b45f888f49f00c2
Nikhil Raj <rajnikhil870@gmail.com>
2018-03-02 12:31:01 +0530

    feeding to the database

--- feed.php
+++ feed.php
@@ -1,3 +1,15 @@
 <?php
-echo "Email : ".$_POST['email']." and description : ".$_POST['description'];
+echo "Email : ".$_GET['email']." and description : ".$_GET['description'];
+$email = $_GET['email'];
+$feed = $_GET['description'];
+
+$db = mysqli_connect("localhost","id4767136_toughnickel","nikhil@443*","id4767136_feedbacks") or die("Failed to connect");
+
+$sql = "INSERT INTO entries (email,feed) values ('$email','$feed')";
+
+if(mysqli_query($db,$sql)){
+    echo "\nRecord Added successfully";
+}else{
+    echo "\nFailed Badly!!";
+}
 ?>
